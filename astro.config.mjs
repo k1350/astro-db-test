ac383c225f2cc376c62690116a8e06809098900c
Kimiko Mitamura <pom96egran46ate@gmail.com>
2024-04-14 16:45:13 +0900

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import preact from "@astrojs/preact";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), preact()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
